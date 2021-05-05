e3cb43bdb3ce3ac49b2966c23d42bfdf3fea5932
harsha Murupudi <itsmeeeharsha@gmail.com>
2021-05-05 10:43:46 +0530

    ADD: Initial help message prompting the users to use keyboard arrow keys

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -9,6 +9,8 @@ import LeaderBoard from '../../components/leader-board/leader-board.component'
 import addNumber from '../../utils/addNumber'
 import { addnewNumber } from '../../redux/action'
 
+import { IoCaretUp, IoCaretForward, IoCaretDown, IoCaretBack } from "react-icons/io5";
+
 import './home.styles.scss'
 
 const HomePage = (props) => {
@@ -25,28 +27,53 @@ const HomePage = (props) => {
       props.dispatch(addnewNumber(newData));
     }
   };
-  return (
-    <section className="home_parent_conatiner">
-      <div className="home_contanier">
-        <div className="game_container">
-          <ScoreSection />
-          <div className="game_dispciption">
-            {" "}
-          Join the numbers and get to <b>2048 title!</b>{" "}
-            <button onClick={handleResetGame}>Reset</button>
+
+  const renderIntitalMessage = () => {
+    if (!(props.scores.length > 0)) {
+      return (
+        <div className="initial_message">
+          <h3>Use keyboard to play</h3>
+          <div className="blink">
+            <div><IoCaretUp /></div>
+            <div className="right_down_left_arrows_container">
+              <IoCaretBack />
+              <IoCaretDown />
+              <IoCaretForward />
+            </div>
           </div>
-          <GameBoard />
         </div>
-        <ReplaySection />
-        <LeaderBoard />
-      </div>
-    </section>
+      )
+    } else {
+      return <></>
+    }
+  }
+
+  return (
+    <div>
+      <section className="home_parent_conatiner">
+        {renderIntitalMessage()}
+        <div className="home_contanier">
+          <div className="game_container">
+            <ScoreSection />
+            <div className="game_dispciption">
+              {" "}
+          Join the numbers and get to <b>2048 title!</b>{" "}
+              <button onClick={handleResetGame}>Reset</button>
+            </div>
+            <GameBoard />
+          </div>
+          <ReplaySection />
+          <LeaderBoard />
+        </div>
+      </section>
+    </div>
   )
 }
 
 function mapStateToProps(state) {
   return {
     replay: state.gameInfo.replay,
+    scores: state.leaderBoard.scores || []
   };
 }
 
